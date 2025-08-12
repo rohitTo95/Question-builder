e6b2e86a860334af0991250ce61d03f24aac9173
“rohitTo95” <xman76066@gmail.com>
2025-08-12 22:17:02 +0530

    refactor: Remove redundant comment in ProtectedRoute component

--- client/src/components/auth/ProtectedRoute.tsx
+++ client/src/components/auth/ProtectedRoute.tsx
@@ -21,7 +21,6 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   }
 
   if (!isAuthenticated) {
-    // Redirect to login page with return url
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
